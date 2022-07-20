46adaec1584d638160f6025366f0563ceaa1ef2a
username <greenrock46@gmail.com>
2022-07-20 14:24:33 +0900

    Feat: 시퀄라이즈 메인파일에 연동 * /index.ts: 시퀄라이즈 연동 코드 추가

--- index.ts
+++ index.ts
@@ -1,13 +1,29 @@
 import express from 'express';
 import morgan from 'morgan';
+import { sequelize } from './models';
 
 const app = express();
 
 const prod: boolean = process.env.NODE_ENV === 'production';
 app.set('port', prod ? process.env.PORT : 3333);
 
+// 시퀄라이즈 연동
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공!');
+  })
+  .catch((err: Error) => {
+    console.error(err);
+  });
+
+if (prod) {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('백엔드 정상 동작!');
