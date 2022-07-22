f7471514b3df1b56d181a62fe0e17d7f479cf6f1
username <greenrock46@gmail.com>
2022-07-22 22:40:41 +0900

    Feat: 게시글 삭제 api 로직 구현     * post.router.ts: DELETE /api/posts/:postId 라우터 등록     * post.ctrl.ts: 게시글 삭제 비즈니스 로직 구현

--- routes/post.router.ts
+++ routes/post.router.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get('/', authenticateToken, PostController.createPost);
 router.patch('/:postId', authenticateToken, PostController.setPost);
+router.delete('/:postId', authenticateToken, PostController.deletePost);
 
 export default router;
